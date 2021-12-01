d26e0ed16bb00c466212aaefe35e8082dd867435
RicciGiovanni <91595816+RicciGiovanni@users.noreply.github.com>
2021-12-01 18:37:48 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,25 @@
-
+#include "lib.h"
+bool conversione(char &a)
+{
+  bool v=true;
+  if(a>=65 and a<=90 or a>=97 and a<=122)
+  {
+    v=true;
+    if(a>=65 and a<=90)
+    {
+      a=a+32;
+      return a;
+    }
+    else
+    {
+      a=a-32;
+      return a;
+    }
+  }
+  else
+  {
+    v=false;
+  }
+  return v;
+}
+    
