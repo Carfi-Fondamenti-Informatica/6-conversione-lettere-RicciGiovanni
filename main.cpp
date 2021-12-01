cb0c1ebd06b48ff932622813353e202087a81b93
RicciGiovanni <91595816+RicciGiovanni@users.noreply.github.com>
2021-12-01 18:44:55 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
-
+#include "lib.h"
+using namespace std;
 int main(){
+  char a=a;
+  cin>>a;
+  if(conversione(a)==false)
+  {
+    cout<<errore<<endl;
+  }
+  else
+  {
+    cout<<a<<endl;
+  }
+  
   return 0;
 }
